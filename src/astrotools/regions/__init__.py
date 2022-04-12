6111bd7807ca8e5fe4a8fb496d7e3e5ca95ef574
Fabian Scheuermann <schmnn+gth@posteo.co>
2022-04-12 09:55:23 +0200

    2022.04.12 - add find_sky_region

--- src/astrotools/regions/__init__.py
+++ src/astrotools/regions/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-from .core import Regions
+from .core import Regions, find_sky_region
 
-__all__ = ['Regions']
+__all__ = ['Regions','find_sky_region']
--- src/astrotools/regions/core.py
+++ src/astrotools/regions/core.py
@@ -11,6 +11,9 @@ from skimage.measure import regionprops, find_contours
 
 from .utils import resolution_from_wcs
 
+from regions import PixCoord, PolygonPixelRegion
+from functools import reduce
+
 logger = logging.getLogger(__name__)
 
 class Regions:
@@ -225,9 +228,29 @@ def find_region(image,plot=False):
         plt.show()
         
 
-from skimage.measure import find_contours
-from regions import PixCoord, PolygonPixelRegion
-from functools import reduce
+def find_sky_region(mask,wcs):
+    '''create a region object from a mask and wcs
+    
+    Returns:
+    reg_pix : PixelRegion
+    reg_sky : SkyRegion
+    '''
+
+    mask[:,[0,-1]]=1
+    mask[[0,-1],:]=1
+
+    # find the contours around the image to use as vertices for the PixelRegion
+    contours = find_contours(mask,0.5,)
+    # we use the region with the most vertices
+    coords = max(contours,key=len)
+    #coords = np.concatenate(contours)
+
+    # the coordinates from find_counters are switched compared to astropy
+    reg_pix  = PolygonPixelRegion(vertices = PixCoord(*coords.T[::-1])) 
+    reg_sky  = reg_pix.to_sky(wcs)
+    
+    return reg_pix, reg_sky
+
 
 def region_from_mask(mask):
     
